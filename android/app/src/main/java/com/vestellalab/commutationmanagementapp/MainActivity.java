25ec7496c02a8a09d27f9c9d998755a947aed3b7
이영수 <yslee9311@gmail.com>
2023-03-17 13:22:00 +0900

    Android 및 iOS 환경 설정(앱 이름, 번들ID, 버전)

--- android/app/src/main/java/com/vestellalab/commutationmanagementapp/MainActivity.java
+++ android/app/src/main/java/com/vestellalab/commutationmanagementapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.commutationmanagementapp;
+package com.vestellalab.commutationmanagementapp;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/vestellalab/commutationmanagementapp/MainApplication.java
+++ android/app/src/main/java/com/vestellalab/commutationmanagementapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.commutationmanagementapp;
+package com.vestellalab.commutationmanagementapp;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
