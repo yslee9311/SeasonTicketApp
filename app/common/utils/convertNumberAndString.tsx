d6bbcb2cf8ab62153033d1315cf753db8817217d
이영수 <yslee9311@gmail.com>
2023-03-23 11:45:58 +0900

    정기권 PurchaseDetails (결제 시 차량 정보 입력) 페이지에서 Context 연동 및 입력한 정보들을 각 형식에 맞게 검토 및 서버로 데이터 추가 연동 & 차량 번호 검토 함수 생성

--- app/common/utils/convertNumberAndString.tsx
+++ app/common/utils/convertNumberAndString.tsx
@@ -2,3 +2,30 @@
 export const numberWithCommas = (x: number) => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+export const checkCarNumberString = (carNum: string) => new Promise<string>((resolve, reject) => {
+    let isNumeric = (data: string): boolean => { return !isNaN(Number(data)) }
+    let nonSpaceString = carNum.replace(/ /gi, ''); // 중간에 띄어쓰기를 한 경우 해당 띄어쓰기 제거
+    let correctCarNum = true;
+    if (nonSpaceString.length >= 7 && nonSpaceString.length <= 8) {  // 7자리 이상이어야 됨.
+        for (let i = nonSpaceString.length; i > 0; i--) { // 뒤에서부터
+            if (i == nonSpaceString.length - 4) {  // 뒤에서 5번째 자리의 경우
+                if (isNumeric(nonSpaceString.substring(i - 1, i))) { // 문자인지 확인
+                    correctCarNum = false;
+                }
+            } else {
+                if (!isNumeric(nonSpaceString.substring(i - 1, i))) { // 숫자인지 확인
+                    correctCarNum = false;
+                }
+            }
+        }
+    } else { // 6자리 이하인 경우는 없으므로 이 경우 false
+        correctCarNum = false;
+    }
+    if (correctCarNum == true) {
+        carNum = [nonSpaceString.slice(0, nonSpaceString.length - 4), ' ', nonSpaceString.slice(nonSpaceString.length - 4)].join('')
+        resolve(carNum);
+    } else {
+        resolve("");
+    }
+})
--- app/components/pages/stPurchaseDetails/StPurchaseDetails.tsx
+++ app/components/pages/stPurchaseDetails/StPurchaseDetails.tsx
@@ -4,22 +4,31 @@ import {
     Platform,
     StatusBar,
     View,
+    Alert,
 } from 'react-native';
+import moment from 'moment';
 
 import colors from '../../../common/values/colors';
+import { checkCarNumberString } from '../../../common/utils/convertNumberAndString';
+import { postVehicleInfo, postPaymentInfo } from '../../../common/utils/server/postServer'
 
 import HeaderTab from '../../molecules/headerTab';
 import StDetailsInfo from '../../organisms/stPurchase/stDetailsInfo';
 import BottomButton from '../../atoms/button/bottomButton';
+import { ContextConsumer } from '../../../config/contexts';
 
 type MyProps = {
     navigation: any
+    context: any
     route: any
 };
-type MyState = {};
+type MyState = {
+    targetCarNum: string
+    targetName: string
+    targetPhoneNum: string
+};
 
-export default class StPurchaseDetails extends React.Component<MyProps, MyState> {
-
+class StPurchaseDetails extends React.Component<MyProps, MyState> {
     constructor(props: any) {
         super(props);
         this.state = {
@@ -28,9 +37,60 @@ export default class StPurchaseDetails extends React.Component<MyProps, MyState>
             targetPhoneNum: '',
         };
     }
-    componentDidMount(): void {
-        console.log(this.props.route.params);
-    }
+
+    purchaseFunc = () => new Promise(async (resolve, reject) => {
+        let {
+            targetCarNum,
+            targetName,
+            targetPhoneNum,
+        } = this.state;
+        targetCarNum = await checkCarNumberString(targetCarNum) // 차량 번호 여부 검토 및 한글 뒤 띄어쓰기 추가
+        targetPhoneNum = [targetPhoneNum.slice(0, 3), '-', targetPhoneNum.slice(3, 7), '-', targetPhoneNum.slice(7)].join('') // 휴대폰 번호의 형식으로 변경
+
+        if (targetCarNum != "" && targetPhoneNum.length == 13) { // 차량 정보 및 이름, 휴대폰 번호가 일치하는지
+            const inputVehicleData = {
+                plate_number: targetCarNum,
+                name: targetName,
+                phone: targetPhoneNum
+            }
+            // console.log('inputVehicleData', inputVehicleData);
+            let outputVehicleData = await postVehicleInfo(inputVehicleData) // 차량 정보 등록
+
+            const { label, price } = this.props.route.params
+            let startDay = moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
+            let endDay = moment(new Date().getTime() + 1000 * 60 * 60 * 24 * 30).format('YYYY-MM-DD HH:mm:ss')
+            const inputPaymentData = {
+                ticket_vehicle_uuid_bin: outputVehicleData.uuid,
+                goods: label,
+                start_on: startDay,
+                end_on: endDay,
+                card_number: "3298400198741982",
+                amount: price.toString(),
+                payment_on: startDay,
+            }
+            // console.log('inputPaymentData', inputPaymentData);
+            let boolSuccess = await postPaymentInfo(inputPaymentData) // 해당 차량에 대한 결제 정보 등록
+            if (boolSuccess) {
+                this.props.navigation.navigate("StPurchaseCompleted", {})
+            } else {
+                Alert.alert(
+                    '결제 오류',
+                    '결제 중 오류가 발생하였습니다. 처음 화면에서 다시 결제를 진행해주세요.',
+                    [
+                        { text: "확인", style: 'cancel', onPress: () => this.props.navigation.popToTop() },
+                    ]
+                );
+            }
+        } else {
+            Alert.alert(
+                '잘못된 차량 정보',
+                '입력하신 차량 정보 중 잘못 입력된 정보가 있는 것으로 확인됩니다. 재입력 후 "구매하기" 버튼을 눌러주세요',
+                [
+                    { text: "확인", style: 'cancel', onPress: () => { } },
+                ]
+            );
+        }
+    })
 
     render() {
         const {
@@ -62,9 +122,30 @@ export default class StPurchaseDetails extends React.Component<MyProps, MyState>
                     weight={'Bold'}
                     textColor={colors.white}
                     buttonColor={colors.black}
-                    func={() => this.props.navigation.navigate("StPurchaseCompleted", {})}
+                    func={() => this.purchaseFunc()}
                 />
             </View>
         )
     }
 }
+
+const StPurchaseDetailsContainer = (props: {
+    navigation: any;
+    route: any;
+}) => {
+    return (
+        <ContextConsumer>
+            {
+                (context) => (
+                    <StPurchaseDetails
+                        navigation={props.navigation}
+                        route={props.route}
+                        context={context}
+                    />
+                )
+            }
+        </ContextConsumer>
+    )
+}
+
+export default StPurchaseDetailsContainer;
