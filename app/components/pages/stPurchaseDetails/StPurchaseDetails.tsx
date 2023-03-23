00557d7a9af75c04c6275ee598d40591eb92657a
이영수 <yslee9311@gmail.com>
2023-03-23 15:31:41 +0900

    정기권 purchase (구매) 페이지에서 구매 진행 시, 기존에 등록된 차량 번호인지를 판단하여, 등록되어 있던 데이터인 경우, 연장 페이지로 이동하도록 안내하고 구매는 취소

--- app/components/pages/stPurchaseDetails/StPurchaseDetails.tsx
+++ app/components/pages/stPurchaseDetails/StPurchaseDetails.tsx
@@ -48,38 +48,49 @@ class StPurchaseDetails extends React.Component<MyProps, MyState> {
         targetPhoneNum = [targetPhoneNum.slice(0, 3), '-', targetPhoneNum.slice(3, 7), '-', targetPhoneNum.slice(7)].join('') // 휴대폰 번호의 형식으로 변경
 
         if (targetCarNum != "" && targetPhoneNum.length == 13) { // 차량 정보 및 이름, 휴대폰 번호가 일치하는지
-            const inputVehicleData = {
-                plate_number: targetCarNum,
-                name: targetName,
-                phone: targetPhoneNum
-            }
-            // console.log('inputVehicleData', inputVehicleData);
-            let outputVehicleData = await postVehicleInfo(inputVehicleData) // 차량 정보 등록
-
-            const { label, price } = this.props.route.params
-            let startDay = moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
-            let endDay = moment(new Date().getTime() + 1000 * 60 * 60 * 24 * 30).format('YYYY-MM-DD HH:mm:ss')
-            const inputPaymentData = {
-                ticket_vehicle_uuid_bin: outputVehicleData.uuid,
-                goods: label,
-                start_on: startDay,
-                end_on: endDay,
-                card_number: "3298400198741982",
-                amount: price.toString(),
-                payment_on: startDay,
-            }
-            // console.log('inputPaymentData', inputPaymentData);
-            let boolSuccess = await postPaymentInfo(inputPaymentData) // 해당 차량에 대한 결제 정보 등록
-            if (boolSuccess) {
-                this.props.navigation.navigate("StPurchaseCompleted", {})
-            } else {
+            let duplicateBool = await this.checkDuplicate(targetCarNum)
+            if (duplicateBool) { // 기존의 데이터들 중, 차량 번호가 중복인 경우
                 Alert.alert(
-                    '결제 오류',
-                    '결제 중 오류가 발생하였습니다. 처음 화면에서 다시 결제를 진행해주세요.',
+                    '잘못된 차량 정보',
+                    '이미 정기권이 구매된 차량 번호입니다. 기존에 등록된 차량의 경우 정기권 연장을 진행해주세요.',
                     [
                         { text: "확인", style: 'cancel', onPress: () => this.props.navigation.popToTop() },
                     ]
                 );
+            } else {
+                const inputVehicleData = {
+                    plate_number: targetCarNum,
+                    name: targetName,
+                    phone: targetPhoneNum
+                }
+                // console.log('inputVehicleData', inputVehicleData);
+                let outputVehicleData = await postVehicleInfo(inputVehicleData) // 차량 정보 등록
+
+                const { label, price } = this.props.route.params
+                let startDay = moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
+                let endDay = moment(new Date().getTime() + 1000 * 60 * 60 * 24 * 30).format('YYYY-MM-DD HH:mm:ss')
+                const inputPaymentData = {
+                    ticket_vehicle_uuid_bin: outputVehicleData.uuid,
+                    goods: label,
+                    start_on: startDay,
+                    end_on: endDay,
+                    card_number: "3298400198741982",
+                    amount: price.toString(),
+                    payment_on: startDay,
+                }
+                // console.log('inputPaymentData', inputPaymentData);
+                let boolSuccess = await postPaymentInfo(inputPaymentData) // 해당 차량에 대한 결제 정보 등록
+                if (boolSuccess) {
+                    this.props.navigation.navigate("StPurchaseCompleted", {})
+                } else {
+                    Alert.alert(
+                        '결제 오류',
+                        '결제 중 오류가 발생하였습니다. 처음 화면에서 다시 결제를 진행해주세요.',
+                        [
+                            { text: "확인", style: 'cancel', onPress: () => this.props.navigation.popToTop() },
+                        ]
+                    );
+                }
             }
         } else {
             Alert.alert(
@@ -90,6 +101,17 @@ class StPurchaseDetails extends React.Component<MyProps, MyState> {
                 ]
             );
         }
+    })
+
+    checkDuplicate = (newPlateNumber: string) => new Promise<boolean>(async (resolve, reject) => {
+        let { totalInfo } = this.props.context.state
+        let duplicateBool = false
+        for (const value of totalInfo) {
+            if (newPlateNumber == value.plate_number) {
+                duplicateBool = true
+            }
+        }
+        resolve(duplicateBool)
     })
 
     render() {
